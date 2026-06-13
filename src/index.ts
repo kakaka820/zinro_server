9c6a72f89b26cdc30fdd34249d17e014b419983c
kakaka820 <tata_ta@163.com>
2026-06-13 20:52:31 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -47,6 +47,7 @@ app.use(session({
   cookie: {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
+    sameSite: 'lax',
     maxAge: 1000 * 60 * 60 * 24 * 7, // 7日間
   },
 }));
